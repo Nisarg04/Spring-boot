6f5bf0b947f622ca67f3213690f98616ee0376cb
Nisarg Patil <nisargkumar.s.patil@accenture.com>
2018-07-31 16:17:35 +0530

    added other CRUD operations

---
+++ lkm-jpa/lkm-jpa/src/main/java/com/accenture/lkm/entity/EmployeeEntity.java
@@ -0,0 +1,51 @@
+package com.accenture.lkm.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Employee")
+public class EmployeeEntity {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int employeeId;
+	private String employeeName;
+	private double salary;
+	private Integer departmentCode;
+	
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Integer getDepartmentCode() {
+		return departmentCode;
+	}
+	public void setDepartmentCode(Integer departmentCode) {
+		this.departmentCode = departmentCode;
+	}
+	@Override
+	public String toString() {
+		return "EmployeeEntity [employeeId=" + employeeId + ", employeeName=" + employeeName + ", salary=" + salary
+				+ ", departmentCode=" + departmentCode + "]";
+	}
+
+}
